59e3f2e4132290f6e835fbb09974e76d3ab65ddf
Alexander Shershen <alexander.shershen@inbox.ru>
2020-02-11 01:47:18 +0300

    first version

---
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    public int count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        count = 0;
+    }
+
+    public void addOne(View view) {
+        count++;
+        textView.setText(Integer.toString(count));
+    }
+}
